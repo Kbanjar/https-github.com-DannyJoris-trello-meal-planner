90fb21987802f232f7f8d92cb6d76b2cf3179209
DannyJoris <danny_joris@yahoo.co.uk>
2017-03-13 01:05:15 -0300

    Revert to popup auth

--- js/scripts.js
+++ js/scripts.js
@@ -44,7 +44,7 @@ $(document).ready(function() {
       $('.authenticate__link').on('click', function(e) {
         e.preventDefault();
         Trello.authorize({
-          type: 'redirect',
+          type: 'popup',
           name: 'Trello Meal Planner',
           scope: {
             read: 'true',
